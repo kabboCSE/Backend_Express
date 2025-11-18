7afeb40a0e68985dbbad142441eb69e60ae40bbe
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-11-19 01:55:05 +0600

    _

--- smart-deals-client/src/components/Product/Product.jsx
+++ smart-deals-client/src/components/Product/Product.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Product = ({ product }) => {
-  const { title, price_max, price_min, image } = product;
+  const { _id, title, price_max, price_min, image } = product;
   return (
     <div className="card bg-base-100 shadow-sm">
       <figure className="px-4 pt-4">
         <img src={image} alt="Shoes" className="rounded-xl" />
       </figure>
-      <div className="card-body items-center text-center">
+      <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>
-          ${price_min}-{price_max}
+          Price: ${price_min}-{price_max}
         </p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          <Link to={`productDetails/${_id}`} className="btn btn-primary w-full">
+            View Details
+          </Link>
         </div>
       </div>
     </div>
